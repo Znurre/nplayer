92cbd57bfcf70160fb032cb284f163231785fca4
Znurre <znurree@gmail.com>
2016-08-02 17:46:33 +0200

    .artist can now use iterators

--- requests/ArtistRequest.cpp
+++ requests/ArtistRequest.cpp
@@ -2,6 +2,7 @@
 #include "RequestHandler.h"
 #include "RequestInvocationContext.h"
 #include "RequestResponse.h"
+#include "InformationResourceRepository.h"
 
 #include "entities/Artist.h"
 
@@ -12,20 +13,43 @@ QString ArtistRequest::trigger() const
 
 RequestResponse ArtistRequest::invoke(const QStringList &arguments, const QString &who, const RequestInvocationContext &context)
 {
+	const QString &id = arguments.join(QChar::Space);
+
 	InformationResourceRepository &repository = context.informationResourceRepository();
 	IdGenerator &idGenerator = context.idGenerator();
 
-	RequestHandler requestHandler(repository, idGenerator);
+	IInformationResource *resource = repository.get(id);
 
-	Artist *artist = requestHandler
-		.get<Artist>("artist.getInfo"
-			, as::artist = arguments.join(QChar::Space)
-			, as::username = who
-		);
-
-	if (artist)
+	if (resource)
 	{
-		return RequestResponse("templates/Artist.qml", artist);
+		IIterator<Artist> *iterator = resource->iterator<Artist>();
+
+		if (iterator)
+		{
+			Artist *track = iterator->next(repository, idGenerator);
+
+			if (track)
+			{
+				return RequestResponse("templates/Artist.qml", track);
+			}
+
+			return RequestResponse("templates/NoMoreData.qml", nullptr);
+		}
+	}
+	else
+	{
+		RequestHandler requestHandler(repository, idGenerator);
+
+		Artist *artist = requestHandler
+			.get<Artist>("artist.getInfo"
+				, as::artist = arguments.join(QChar::Space)
+				, as::username = who
+			);
+
+		if (artist)
+		{
+			return RequestResponse("templates/Artist.qml", artist);
+		}
 	}
 
 	return RequestResponse();
