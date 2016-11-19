2df4d61c91fe7b42ee059a76bf015b8d843df8b0
Znurre <znurree@gmail.com>
2016-11-19 20:54:21 +0100

    Fixed compilation error

--- RequestRepository.cpp
+++ RequestRepository.cpp
@@ -1,6 +1,5 @@
 #include "RequestRepository.h"
 
-#include "requests/HelloWorldRequest.h"
 #include "requests/NowPlayingRequest.h"
 #include "requests/AlbumRequest.h"
 #include "requests/ArtistRequest.h"
@@ -23,7 +22,6 @@
 
 RequestRepository::RequestRepository()
 {
-	attach<HelloWorldRequest>();
 	attach<NowPlayingRequest>();
 	attach<AlbumRequest>();
 	attach<ArtistRequest>();
